ed193aa838114304a8dc515ab9d0db6858afff2b
OstapKlymochko <klymochkoo@gmail.com>
2024-04-02 00:49:33 +0300

    project init

---
+++ Trade-Pulse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trade_Pulse.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
