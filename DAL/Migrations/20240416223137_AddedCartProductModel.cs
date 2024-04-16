9f00189e7c1f43770f84d854a7f8e0cfbddcd078
OstapKlymochko <klymochkoo@gmail.com>
2024-04-17 01:38:32 +0300

    added CartProduct model

---
+++ DAL/Migrations/20240416223137_AddedCartProductModel.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCartProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_Users_UserId",
+                table: "Cart");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "Cart",
+                newName: "Carts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cart_UserId",
+                table: "Carts",
+                newName: "IX_Carts_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "CartsProducts",
+                columns: table => new
+                {
+                    CartId = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    ItemsCount = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_CartsProducts_Carts_CartId",
+                        column: x => x.CartId,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartsProducts_Carts_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartsProducts_CartId",
+                table: "CartsProducts",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartsProducts_ProductId",
+                table: "CartsProducts",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "CartsProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts");
+
+            migrationBuilder.RenameTable(
+                name: "Carts",
+                newName: "Cart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_UserId",
+                table: "Cart",
+                newName: "IX_Cart_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_Users_UserId",
+                table: "Cart",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ DAL/Tools/CartProduct.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Tools
+{
+    [Keyless]
+    public class CartProduct
+    {
+        public Cart Cart { get; set; } = null!;
+        public Cart Product { get; set; } = null!;
+        public uint ItemsCount { get; set; }
+    }
+}
