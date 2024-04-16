60c176fdccd178088d9232a6bd2d9f851eceeda6
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2024-04-16 16:49:16 +0300

    added new fields

--- BLL/DTOs/ProductDto.cs
+++ BLL/DTOs/ProductDto.cs
@@ -1,11 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BLL.DTOs
 {
     public class ProductDto
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Категорія' є обов'язковим")]
+        public int CategoryId { get; set; }
+
         [Required(ErrorMessage = "Поле 'Назва товару' є обов'язковим")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Опис' є обов'язковим")]
+        public string Description { get; set; }
+
+        public string Model { get; set; }
 
         [Required(ErrorMessage = "Поле 'Ціна' є обов'язковим")]
         [Range(0, double.MaxValue, ErrorMessage = "Ціна повинна бути більше 0")]
@@ -15,5 +26,8 @@ namespace BLL.DTOs
         [Range(1, int.MaxValue, ErrorMessage = "Кількість повинна бути більше 0")]
         public uint ItemsAvailable { get; set; }
 
+        public DateTime CreatedAt { get; set; }
+
+        public int VendorId { get; set; }
     }
 }
--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -1,9 +1,10 @@
 ﻿using BLL.DTOs;
 using BLL.Errors;
-using BLL.Features;
 using BLL.Services.Interfaces;
 using DAL.Repositories.Interfaces;
 using DAL.Tools;
+using System;
+using System.Threading.Tasks;
 
 namespace BLL.Services
 {
@@ -16,26 +17,27 @@ namespace BLL.Services
             _productRepository = productRepository;
         }
 
-
-        //public async Task AddProductAsync(ProductDto productDto)
-        //{
-        //    await _productRepository.AddAsync(productDto);
-        //    await _productRepository.SaveChangesAsync();
-        //}
-
         public async Task<ServiceResult<ProductDto>> AddProductAsync(ProductDto productDto)
         {
             try
             {
                 var product = new Product
                 {
+                    CategoryId = productDto.CategoryId,
                     Title = productDto.Title,
+                    Description = productDto.Description,
+                    Model = productDto.Model,
                     Price = productDto.Price,
-                    ItemsAvailable = productDto.ItemsAvailable
+                    ItemsAvailable = productDto.ItemsAvailable,
+                    CreatedAt = DateTime.Now, 
+                    VendorId = productDto.VendorId 
                 };
 
                 _productRepository.Add(product);
-                 _productRepository.SaveChangesAsync();
+                _productRepository.SaveChangesAsync();
+                //await _productRepository.SaveChangesAsync();
+
+    
 
                 //return ServiceResult<ProductDto>.Success(productDto);
                 return productDto;
@@ -43,7 +45,6 @@ namespace BLL.Services
             catch (Exception ex)
             {
                 return ServiceResult<ProductDto>.Failure(new ModelError($"Failed to add product: {ex.Message}"));
-
             }
         }
     }
